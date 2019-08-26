4b9a7798e4c14544499d488914ee425aba84b36e
NABenning <benning6@msu.edu>
2019-08-26 17:54:51 -0400

    HW day 1 exercise 1

--- day1-homework/day1-exercise1.py
+++ day1-homework/day1-exercise1.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-
+#fields = line.split("\t")
 import sys
 
 counter = 0
 for line in open( sys.argv[1]):
+    fields = line.split("\t")
+    if fields[2] == "*":
+        continue
     counter += 1
 
 print(counter)
