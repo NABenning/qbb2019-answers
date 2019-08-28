fb0958b87f495531c5e2a566994e785aceb88ab8
NABenning <benning6@msu.edu>
2019-08-28 16:55:07 -0400

    day 3 homework exercise

--- day3-lunch/binary_search.py
+++ day3-lunch/binary_search.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
+import numpy
+
+"""Organize input data"""
 
 protein_coding_genes = {}
 
+mid_list = [] #for use in Binary Search
+
 protein_coding_genes_count = 0 
-#str(fields[4]) == gene end
-#str(fields[3]) == gene beginning
  
 for line in open( sys.argv[1] ):
    fields = line.strip("\n").split()
@@ -15,41 +18,74 @@ for line in open( sys.argv[1] ):
    
    gene_beginning = int(fields[3])
    gene_end = int(fields[4])
-   gene_mid = float(gene_beginning + gene_end) / 2
+   gene_mid = (gene_beginning + gene_end) / 2
    for i, mini in enumerate( fields[8:]):
        if "gene_name" in mini:
            gene_name = fields[8 + i + 1]
            break
            
-   #gene_mid = (str(fields[4]) + str(fields[3]))/2
    if "protein_coding" in line:
        if fields[0] == "3R" and fields[2] == "gene":
            protein_coding_genes_count +=1
-           print(gene_beginning , gene_end , gene_mid , gene_name)  
-                                                                            
-           protein_coding_genes.update({str(fields[3]): str(fields[4])})    
+           print(gene_beginning , gene_end , gene_mid , gene_name) 
+           
+           protein_coding_genes.update({str(fields[3]): str(fields[4])})
+           mid_list.append(gene_mid)
+           
+           for i in range(0, len(mid_list)):
+               mid_list[i] = int(mid_list[i])
+               
 
-#print("number of protein coding genes: " + str( protein_coding_genes_count))
-"""list of genes moved into ~/day3-lunch/genes.txt for later use as sys.argv[2]""" 
+
+"""Binary Search """           
+           
+
+startpos = []
+
+length1 = len(startpos)
+mutation = 21378950
+#mutation_test = int(input("try:"))
+
+def binary_search(lst, m):
+   l = len(lst)
+   first = 0
+   last = l-1
+   flag = 0
+   distance = 0
+   iteration = 0
+   while first <= last:
+       mid = int((first + last) / 2)
+       iteration += 1
+       if lst[mid_list] == m:
+           flag = 1
+           break
+       elif lst[mid_list] > m:
+           last = mid_list - 1
+       else:
+           first = mid_list + 1
+
+
+   if flag == 0 and mid_list+1 < l:
+       if lst[mid_list+1] - m >= m - lst[mid_list]:
+           distance = m - lst[mid_list]
+           return lst[mid_list], distance, iteration
+       else:
+           distance = lst[mid_list+1] - m
+           return lst[mid_list+1], distance, iteration
+   elif mid_list + 1 >= l:
+       distance = m - lst[mid_list]
+       return lst[mid_list], distance, iteration
+   else:
+       return lst[mid_list], distance, iteration
 
 
 
-#def ()
- # for i, line in enumerate( open( sys.argv[2] ) ):
-  #    fields = line.strip("\n").split("\t")
- # lo = 0
- # hi = len(line)-1
- # mid = 0
- # number_iterations = 0
- # while (lo <= hi):
- #     mid = (hi+lo)/2
- #     number_iterations = number_iterations + 1
- #     if (search_pos < gene_list[mid][0]):
-          #pick low range
- #     elif (search_pos > gene_list[mid][1]):
-          #pick hi range
-  #    else:
-  #        gene_list[mid]
+answer= binary_search(startpos, mutation)
+key = str(answer[0])
+dis = int(answer[1])
+itera = int(answer[2])
+
+print("nearest gene is ", protein_coding_genes[key],"\n", "disntance=", dis,"bp\n", "iteration=", itera)
 
         
     
