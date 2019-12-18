520f874a57ac4528a5c7fbe4824ed2fc8f414375
NABenning <benning6@msu.edu>
2019-12-18 17:00:58 -0500

    Week 9

---
+++ week9lab/density.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+import seaborn as sns
+
+f = open(sys.argv[1])
+
+locations = []
+
+for line in f:
+    if line.startswith("motif_id"):
+        continue
+    col = line.rstrip("\n").split()
+    if col != []:
+        locations.append((col[3]))
+
+locations = locations[:-3]
+locations = [int(x) for x in locations]
+    
+fig, ax = plt.subplots()    
+ax = sns.distplot(a=locations, bins = 10)
+
+fig.savefig("densityplot.png")
+plt.close(fig)
